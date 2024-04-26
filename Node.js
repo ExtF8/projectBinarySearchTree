be91ff5377349b8f3a53c4f6bc01476d4994ec4e
extf8 <extfield8@gmail.com>
2024-04-26 20:23:07 +0300

    feat: add JSDoc comments to Node class

--- Node.js
+++ Node.js
@@ -1,4 +1,11 @@
+/**
+ * Represents a node in a binary search tree.
+ */
 export default class Node {
+    /**
+     * Creates a new instance of the Node class.
+     * @param {*} data - The data to be stored in the node.
+     */
     constructor(data) {
         this.data = data;
         this.left = null;
